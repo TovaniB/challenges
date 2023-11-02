c7c314b179fe00a14b1a2881bef2927428cc4cec
TovaniBE <tovanigmx.net>
2023-11-02 13:39:30 +0100

    finished challenge

--- js-variables-and-numbers_social-media-post/js/index.js
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -9,14 +9,34 @@ console.clear();
 	- a boolean called isReported
 */
 
-// --v-- write your code here --v--
+const title = "Lollypops";
 
-// --^-- write your code here --^--
+const text = "Lollypops taste nice.";
 
+let numberOfLikes = 6;
+numberOfLikes = numberOfLikes + 1;
+
+const UserOfPost = "Stella";
+
+const isReported = true;
+
+const socialMediaPost = [title, text, numberOfLikes, UserOfPost, isReported];
 /*
 2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
 */
 
 // --v-- write your code here --v--
+/*
+console.log("title", title)
+
+console.log("text", text)
+
+console.log("numberOfLikes", numberOfLikes)
+
+console.log("UserOfPost", UserOfPost)
+
+console.log("isReported", isReported) */
+
+console.log("socialMediaPost", socialMediaPost);
 
 // --^-- write your code here --^--
